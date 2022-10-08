76ec311e208dc597eb90ff2a97d61ffc0a546f1d
django-wong <me@djangowong.com>
2022-10-08 19:04:14 +0800

    update instances.json

--- dist/service/fan_control.d.ts
+++ dist/service/fan_control.d.ts
@@ -1,7 +1,7 @@
 import AbstractService, { Property } from "./abstract";
 import { Nullable } from "homebridge";
 export declare function onToActive(value: Nullable<boolean>): 1 | 0;
-export declare function onToFanState(value: Nullable<boolean>): 0 | 2;
+export declare function onToFanState(value: Nullable<boolean>): 2 | 0;
 export declare function fanLevelToFanState(value: Nullable<number>): 1 | 0;
 export declare class FanControl extends AbstractService {
     static urn: string;
